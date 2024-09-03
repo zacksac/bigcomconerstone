5a540715c30aea3951dce4f14a77634d83bfddb4
unknown <zacksac@gmail.com>
2024-09-03 19:14:58 +0530

    st

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -35,3 +35,31 @@ $('.explore').mouseenter(function () {
 
 
 }
+
+
+var swiper = new Swiper('.swiper1', {
+  // Default parameters
+  slidesPerView: 6,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 2000,
+  },
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 6,
+      spaceBetween: 40
+    }
+  }
+})
